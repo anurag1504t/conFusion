1590ccf2b8aa53e0beb6d609d9e660ad9866a5a1
Anurag Tiwari <anurag1504t@gmail.com>
2020-06-23 23:29:13 +0530

    Passport

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-/*
-Author : Anurag Tiwari
-file_name : app.js
-*/
-
-// Imported Modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -12,6 +6,8 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 // Mounting Express Routes
 var indexRouter = require('./routes/index');
@@ -52,26 +48,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-    console.log(req.session);
+  console.log(req.user);
 
-  if(!req.session.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 
